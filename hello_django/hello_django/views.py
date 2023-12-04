19c8d501070480d3e79b67b4932efb14a6aac8e3
VishnyakAlena <vishnyak-elena@mail.ru>
2023-12-04 19:21:03 +0300

    start-files changes_home.html

--- hello_django/hello_django/views.py
+++ hello_django/hello_django/views.py
@@ -4,4 +4,4 @@ def about(request):
     a = 5 + 6
     return render(request, 'about.html', {'gretting': a})
 def home(request):
-    return HttpResponse('This is my home')
+    return render(request, 'home.html')
